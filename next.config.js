07547054c70c445e5d4c554a8af563a6f85121d1
marwa <marwajawad19@gmail.com>
2026-02-21 01:29:07 +0400

    fix the build issue

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -5,6 +7,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.alias["@splinetool/react-spline"] = path.resolve(
+      "./node_modules/@splinetool/react-spline/dist/react-spline.js"
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
